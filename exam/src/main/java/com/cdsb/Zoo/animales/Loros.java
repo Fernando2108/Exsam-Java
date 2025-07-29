461a3750590a71cc9e3f8e5b30a408f6ddacc202
Fernando <fernandoregidor21@gmail.com>
2025-07-29 13:55:07 +0200

    Add project

--- exam/src/main/java/com/cdsb/Zoo/animales/Loros.java
+++ exam/src/main/java/com/cdsb/Zoo/animales/Loros.java
@@ -1,8 +1,18 @@
 package com.cdsb.Zoo.animales;
 
 import com.cdsb.Zoo.enums.Familia;
-import com.cdsb.Zoo.enums.animales;
+import com.cdsb.Zoo.enums.Animales;
 
-public class Loros extends Animales{
+public class Loros extends AnimalesFamilia {
+
+    Familia = Familia.Aves;
+		incompatibles.add(Animales.ELEFANTE);
+        incompatibles.add(Animales.SERPIENTES);
+        incompatibles.add(Animales.IGUANAS);
+        incompatibles.add(Animales.FOCAS);
+        incompatibles.add(Animales.TUCANES);
+        incompatibles.add(Animales.DELFINES);
+        incompatibles.add(Animales.ZEBRAS);
+        incompatibles.add(Animales.LOROS);
 
 }
